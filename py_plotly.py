93a67562ef0ffdfe6aee4966db25113cd3673cb0
Daniel Guo <mrsep18th@gmail.com>
2019-11-26 23:10:39 +0800

    chart with plotly

--- py_plotly.py
+++ py_plotly.py
@@ -8,9 +8,11 @@ po.init_notebook_mode(connected=True)
 #https://plot.ly/python/bar-charts/
 def ChartBar(x_list, y_list, title = ""):
     fig = go.Figure(
-        data=[go.Bar(x=x_list, y=y_list)],
+        data=[go.Bar(x=x_list, y=y_list, color='lifeExp')],
         layout_title_text=title
     )
     po.iplot(fig)
 
-#def ChartBarExp(data, xLabelList, yLabelList):
+def ChartBarExp(data, xLabel, yLabel, colorLabel=None):
+    fig = px.bar(data, x=xLabel, y=xLabel, color=colorLabel)
+    fig.show()
