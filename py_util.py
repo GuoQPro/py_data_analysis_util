63659ce38450bc2fd518ab7680e8ddcd1bfb35c7
MrSep <mrsep18th@gmail.com>
2019-11-27 18:04:40 +0800

    remove unnecessary semicolon

--- py_util.py
+++ py_util.py
@@ -1,31 +1,31 @@
 
-import math;
-import os;
-import datetime, calendar;
-import platform;
-import pycountry;
-import pandas as pd;
+import math
+import os
+import datetime, calendar
+import platform
+import pycountry
+import pandas as pd
 from .py_macosfile import pickle_load, pickle_dump 
 
 
 def isstring(value):
-    return isinstance(value, str);
+    return isinstance(value, str)
 
 def isnan(value):
     if isstring(value):
         if value.lower() == "nan":
-            return True;
+            return True
         else:
-            return False;
+            return False
     else:
-        return math.isnan(float(value));
+        return math.isnan(float(value))
 
 def check_file_existence(filename):
-    return os.path.isfile(filename);
+    return os.path.isfile(filename)
 
 def traverse_folder(root_path):
     file_list = []
-    for dirName, subdirList, fileList in os.walk(root_path):
+    for dirName, _, fileList in os.walk(root_path):
         for fname in fileList:
             full_path = "{}/{}".format(dirName, fname)
             file_list.append(full_path)
@@ -35,16 +35,16 @@ def traverse_folder(root_path):
 # serialize any binary object to a file.
 #######################################################################################################
 def serialize_object(obj, filename):
-    pickle_dump(obj, filename);
+    pickle_dump(obj, filename)
 
 #######################################################################################################
 # deserialize a file into a binary object.
 #######################################################################################################
 def load_object(filename):
     if check_file_existence(filename):
-        return pickle_load(filename);
+        return pickle_load(filename)
     else:
-        print("cannot find file %s" % filename);
+        print("cannot find file %s" % filename)
 
 #######################################################################################################
 # convert a date string with format "mm/dd/yy" to the nth day of the given year.
@@ -54,22 +54,22 @@ def convert_datestring_to_day_num(datestring, base_y = 19):
     y = int(sec[2])
     m = int(sec[0])
     d = int(sec[1])
-    date = datetime.datetime(y, m, d);
-    day_num = int(date.strftime("%j"));
+    date = datetime.datetime(y, m, d)
+    day_num = int(date.strftime("%j"))
 
     for year in range(base_y, y):
-        day_num += (365 + (1 if calendar.isleap(year) else 0));
+        day_num += (365 + (1 if calendar.isleap(year) else 0))
 
-    return day_num;
+    return day_num
 
 #######################################################################
 # operation system 
 #######################################################################
 def get_os_name():
-    return platform.system();
+    return platform.system()
 
 def is_os_win():
-    return get_os_name() == "Windows";
+    return get_os_name() == "Windows"
 
 
 #######################################################################
@@ -77,10 +77,10 @@ def is_os_win():
 # ISO 3166
 #######################################################################
 def get_country_name_by_alpha2_code(country_alpha2_code):
-    return pycountry.countries.get(alpha_2 = country_alpha2_code).name;
+    return pycountry.countries.get(alpha_2 = country_alpha2_code).name
 
 def get_country_name_by_alpha3_code(country_alpha3_code):
-    return pycountry.countries.get(alpha_3 = country_alpha3_code).name;
+    return pycountry.countries.get(alpha_3 = country_alpha3_code).name
 
 def rectify_country_name_for_pyechart(country_name):
     name_table = {
@@ -94,9 +94,9 @@ def rectify_country_name_for_pyechart(country_name):
     }
 
     if country_name in name_table:
-        return name_table[country_name];
+        return name_table[country_name]
     else:
-        return country_name;
+        return country_name
 
 
 #######################################################################
