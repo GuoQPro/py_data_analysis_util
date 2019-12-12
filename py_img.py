8ba18dec0468f56e64d67f76b26feefed737f98e
MrSep <mrsep18th@gmail.com>
2019-12-12 16:48:38 +0800

    fix warnings

--- py_img.py
+++ py_img.py
@@ -1,53 +1,51 @@
 
 #import matplotlib.image as mlimg;
-import matplotlib.pyplot as plt;
-import math;
-from PIL import Image, ImageDraw;
-from zipfile import ZipFile;
+import math
+from zipfile import ZipFile
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
-#
-#
 def load_img(file_path):
     try:
-        img = Image.open(file_path);#mlimg.imread(str(file_path));
+        img = Image.open(file_path)#mlimg.imread(str(file_path));
     except IOError as err:
-        print("load_img failed: ", err);
+        print("load_img failed: ", err)
     else:
-        img.load();
-        return img;
+        img.load()
+        return img
 
-    return None;
+    return None
 
 def load_img_from_zip(zip_file_full_name):
-    img_list = [];
+    img_list = []
     with ZipFile(zip_file_full_name) as archive:
         for entry in archive.infolist():
             with archive.open(entry) as file:
                 try:
-                    img = Image.open(file);
+                    img = Image.open(file)
                 except IOError as err:
-                    print("load_img_from_zip failed: %s" % err);
+                    print("load_img_from_zip failed: {}".format(err))
                 else:
-                    img_list.append(img);
+                    img_list.append(img)
 
-    return img_list;
+    return img_list
 
 def resize_img(img, width, height):
-    return img.resize((width, height));
+    return img.resize((width, height))
 
 def rotate_img(img, angle):
-    return img.rotate(angle, expand = True);
+    return img.rotate(angle, expand=True)
 
 def thumbnailize_img(img, width, height):
-    img.thumbnail((width, height), Image.ANTIALIAS);
+    img.thumbnail((width, height), Image.ANTIALIAS)
 
-def mask_regioin(img, polygon, outline_color, fill_color_RGBA):
-    draw = ImageDraw.Draw(img, "RGBA");
-    draw.polygon(polygon, outline = outline_color, fill = fill_color_RGBA);
-    return img;
+def mask_region(img, polygon, outline_color, fill_color_RGBA):
+    draw = ImageDraw.Draw(img, "RGBA")
+    draw.polygon(polygon, outline=outline_color, fill=fill_color_RGBA)
+    return img
 
 def show_images(img_list, row_num, col_num, title_list = [], show_axe = True):
-    fig, axes = plt.subplots(row_num, col_num, squeeze = False)#, figsize = (5, 5))
+    _, axes = plt.subplots(row_num, col_num, squeeze = False)#, figsize = (5, 5))
     index = 0
     img_num = len(img_list)
     title_num = len(title_list)
@@ -68,11 +66,10 @@ def calc_row_col(total_num):
     sqrt_root = math.sqrt(total_num)
     row = math.floor(sqrt_root)
     col = math.floor(sqrt_root)
-    
     while row * col < total_num:
         if row > col:
-            col = col + 1;
+            col = col + 1
         else:
-            row = row + 1;
+            row = row + 1
 
-    return row, col;
+    return row, col
